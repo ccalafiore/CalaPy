b211a6f4003b39ccd85474bb141bd3af70305869
Carmelo Calafiore <carmelo.calafiore@newcastle.ac.uk>
2023-10-02 16:44:37 +0100

    finished NoiseLayer

--- calapy/ml/models/single_layers/non_trainable.py
+++ calapy/ml/models/single_layers/non_trainable.py
@@ -1,66 +1,40 @@
 
-import random
+
 import numpy as np
 import torch
+from ..model_tools import ModelMethods as CPModelMethods
+from ... import tensor as cp_tensor
 
 
-class Noise:
+class NoiseLayer(CPModelMethods):
 
+    def __init__(self, scale=0.1):
+        """
 
-    def __init__(self, mu=0.0, sigma=1.0):
+        :param scale: the sigma of the noise to be added to each batch input "x" is defined as the sigma of "x"
+            multiplied by the "scale"
+        :type scale: float | int
+        """
 
-        self.mu = mu
-        self.sigma = sigma
+        CPModelMethods.__init__(self=self, device=None)
 
+        self.mu = 0.0
+        # self.sigma = None
+        self.scale = scale
 
     def forward(self, x, generator=None):
-
         """
 
-        :type x: torch.Tensor
-
+        :param x: The batch input which the noise has to be added to.
+        :type x: torch.Tensor | np.ndarray
+        :param generator: The torch generator of the noise values.
         :type generator: torch.Generator | None
-        :rtype: torch.Tensor | list[torch.Tensor] | tuple[torch.Tensor]
+        :return: The noisy tensor.
+        :rtype: torch.Tensor
         """
-
         # :type x: torch.Tensor # | list[torch.Tensor] | tuple[torch.Tensor]
 
-        if isinstance(x, (torch.Tensor, np.ndarray)):
-
-            if self.sigma != 0.0:
-
-                noise = torch.randn(
-                    size=x.shape, generator=generator, dtype=x.dtype, device=x.device, requires_grad=False)
-
-                if self.sigma != 1.0:
-                    noise *= self.sigma
-
-                x += noise
-
-            if self.mu == 0.0:
-                return x
-            else:
-                return x + self.mu
-
-        # elif isinstance(x, (tuple, list)):
-        #     type_x = type(x)
-        #     x = [self(x[i], generator=generator) for i in range(0, len(x), 1)]
-        #     if type_x == tuple:
-        #         x = tuple(x)
-
-        elif isinstance(x, (int, float)):
-
-            if self.sigma == 0.0:
-                if self.mu == 0.0:
-                    return x
-                else:
-                    return x + self.mu
-            else:
-                noise = random.gauss(mu=self.mu, sigma=self.sigma)
-                x += noise
-                return x
-
+        if self.training:
+            return cp_tensor.add_noise(x=x, scale=self.scale, mu=self.mu, generator=generator)
         else:
-            raise TypeError('x')
-
-    __call__ = forward
+            return x
