e7bd2fc8fb08ed865b5dab0dda4e2ca414b5ae60
Carmelo Calafiore <carmelo.calafiore@newcastle.ac.uk>
2023-10-31 16:07:52 +0000

    Update __init__.py

--- calapy/plot/__init__.py
+++ calapy/plot/__init__.py
@@ -1,34 +1,15 @@
+
 from matplotlib import pyplot as plt
 from .save import *
 from .parameters import my_dpi
+import importlib
 
 # todo: fix the size unit of the figures
 
 
-__all__ = ['heatmaps', 'points', 'bars', 'save_figures', 'cmaps', 'colors', 'my_dpi']
+submodules = ['heatmaps', 'points', 'bars', 'cmaps', 'colors']
+others = ['save_figures', 'my_dpi', 'plt']
+__all__ = submodules + others
 
-
-def initiate(names_submodules=None):
-    template_import_string = "from . import "
-    if isinstance(names_submodules, str):
-        import_string_m = template_import_string + names_submodules
-        exec(import_string_m)
-    elif isinstance(names_submodules, (list, tuple)):
-        for name_m in names_submodules:
-            if isinstance(name_m, str):
-                import_string_m = template_import_string + name_m
-            else:
-                import_string_m = template_import_string + str(name_m)
-            exec(import_string_m)
-    elif names_submodules is None:
-        exec('from . import *')
-        # for name_m in __all__:
-        #     if isinstance(name_m, str):
-        #         import_string_m = template_import_string + name_m
-        #     else:
-        #         import_string_m = template_import_string + str(name_m)
-        #     exec(import_string_m)
-    else:
-        raise TypeError(names_submodules)
-
-
+for sub_module_m in submodules:
+    importlib.import_module(name='.' + sub_module_m, package=__package__)
