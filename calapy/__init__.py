bf62e1f4ca1222961279105dc8f76050afbfbb05
Carmelo Calafiore <carmelo.calafiore@newcastle.ac.uk>
2023-11-29 21:11:20 +0000

    v

--- calapy/__init__.py
+++ calapy/__init__.py
@@ -1,9 +1,9 @@
 import datetime
 import importlib
 
-__version__ = '0.1.20.3'
+__version__ = '0.1.20.4'
 
-__release_day__ = 28
+__release_day__ = 29
 __release_month_num__ = 11
 __release_year__ = 2023
 
